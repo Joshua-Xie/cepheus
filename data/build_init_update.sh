d5c67bfdc42b89f55a8f44b9b5460e85f255b1fd
Chris Jones <cj@hanscj.com>
2017-04-23 14:14:29 -0400

    Added dir to base_color

--- data/build_init_update.sh
+++ data/build_init_update.sh
@@ -23,6 +23,8 @@ set -eu
 # Creates the REPO_ROOT env var for everything else to use...
 export REPO_ROOT=$(git rev-parse --show-toplevel)
 
+source $REPO_ROOT/bootstrap/common/base_colors.sh
+
 # Builds the bootstrapping environment based on the templates and data found in this directory.
 
 # NB: Output directories for this process already exists
@@ -32,31 +34,31 @@ export REPO_ROOT=$(git rev-parse --show-toplevel)
 
 # Build from template
 
-echo "====> Generating bootstrap/common/cepheus_build_update.sh..."
+echo_yellow "====> Generating bootstrap/common/cepheus_build_update.sh..."
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/data/templates/bootstrap/common/cepheus_build_update.sh.j2 -o $REPO_ROOT/bootstrap/common/cepheus_build_update.sh
 # NB: Two passes on output for embedded templates...
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/bootstrap/common/cepheus_build_update.sh -o $REPO_ROOT/bootstrap/common/cepheus_build_update.sh
 sudo chmod +x $REPO_ROOT/bootstrap/common/cepheus_build_update.sh
 
-echo "====> Generating bootstrap/common/cepheus_bootstrap_init.sh..."
+echo_yellow "====> Generating bootstrap/common/cepheus_bootstrap_init.sh..."
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/data/templates/bootstrap/common/cepheus_bootstrap_init.sh.j2 -o $REPO_ROOT/bootstrap/common/cepheus_bootstrap_init.sh
 # NB: Two passes on output for embedded templates...
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/bootstrap/common/cepheus_bootstrap_init.sh -o $REPO_ROOT/bootstrap/common/cepheus_bootstrap_init.sh
 sudo chmod +x $REPO_ROOT/bootstrap/common/cepheus_bootstrap_init.sh
 
-echo "====> Generating bootstrap/common/cepheus_backup_rsync.sh..."
+echo_yellow "====> Generating bootstrap/common/cepheus_backup_rsync.sh..."
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/data/templates/bootstrap/common/cepheus_backup_rsync.sh.j2 -o $REPO_ROOT/bootstrap/common/cepheus_backup_rsync.sh
 # NB: Two passes on output for embedded templates...
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/bootstrap/common/cepheus_backup_rsync.sh -o $REPO_ROOT/bootstrap/common/cepheus_backup_rsync.sh
 sudo chmod +x $REPO_ROOT/bootstrap/common/cepheus_backup_rsync.sh
 
-echo "====> Generating bootstrap/common/cepheus_backup_no_rsync.sh..."
+echo_yellow "====> Generating bootstrap/common/cepheus_backup_no_rsync.sh..."
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/data/templates/bootstrap/common/cepheus_backup_no_rsync.sh.j2 -o $REPO_ROOT/bootstrap/common/cepheus_backup_no_rsync.sh
 # NB: Two passes on output for embedded templates...
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/bootstrap/common/cepheus_backup_no_rsync.sh -o $REPO_ROOT/bootstrap/common/cepheus_backup_no_rsync.sh
 sudo chmod +x $REPO_ROOT/bootstrap/common/cepheus_backup_no_rsync.sh
 
-echo "====> Generating bootstrap/common/bootstrap_chef_server.sh..."
+echo_yellow "====> Generating bootstrap/common/bootstrap_chef_server.sh..."
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/data/templates/bootstrap/common/bootstrap_chef_server.sh.j2 -o $REPO_ROOT/bootstrap/common/bootstrap_chef_server.sh
 # NB: Two passes on output for embedded templates...
 $REPO_ROOT/data/templates/template_engine -d $REPO_ROOT/data/$BUILD_LOCATION/$BUILD_DATA_CENTER/build.yaml -i $REPO_ROOT/bootstrap/common/bootstrap_chef_server.sh -o $REPO_ROOT/bootstrap/common/bootstrap_chef_server.sh
